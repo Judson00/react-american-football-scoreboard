20a8d70e483cdcd396a584017e0f3b702fc3c90a
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-09 18:51:02 -0500

    styling changes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ function App() {
 
             <div className="homeButtons">
               {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-              <button className="homeButtons__touchdown" onClick={() => setlionScore(lionScore + 7)}>Home Touchdown</button>
+              <button className="homeButtons__touchdown" onClick={() => setlionScore(lionScore + 6)}>Home Touchdown</button>
+              <button className="homeButtons_extraPoint" onClick={() => setlionScore(lionScore + 1)}>Home Extra Point</button>
+              <button className="homeButtons_twoPoints" onClick={() => setlionScore(lionScore + 2)}>Home Two Points</button>
               <button className="homeButtons__fieldGoal" onClick={() => setlionScore(lionScore + 3)}>Home Field Goal</button>
             </div>
 
@@ -35,7 +37,9 @@ function App() {
             <div className="away__score">{tigerScore}</div>
 
             <div className="awayButtons">
-              <button className="awayButtons__touchdown" onClick={() => settigerScore(tigerScore + 7)}>Away Touchdown</button>
+              <button className="awayButtons__touchdown" onClick={() => settigerScore(tigerScore + 6)}>Away Touchdown</button>
+              <button className="awayButtons_extraPoint" onClick={() => settigerScore(tigerScore + 1)}>Away Extra Point</button>
+              <button className="awayButtons_twoPoints" onClick={() => settigerScore(tigerScore + 2)}>Away Two Points</button>
               <button className="awayButtons__fieldGoal" onClick={() => settigerScore(tigerScore + 3)}>Away Field Goal</button>
             </div>
 
@@ -43,21 +47,6 @@ function App() {
         </div>
         <BottomRow />
       </section>
-
-      {/* <section className="buttons">
-        <div className="homeButtons"> */}
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          {/* <button className="homeButtons__touchdown" onClick={() => setlionScore(lionScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setlionScore(lionScore + 3)}>Home Field Goal</button>
-        </div>
-
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => settigerScore(tigerScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => settigerScore(tigerScore + 3)}>Away Field Goal</button>
-        </div>
-
-      </section> */}
-
     </div>
   );
 }
